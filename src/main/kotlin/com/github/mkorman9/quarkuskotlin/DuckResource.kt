1c098540a23d57b8999eda49b3d244016b27f820
Michal Korman <michal.korman@icloud.com>
2023-12-27 18:36:39 +0100

    don't run DuckResource on virtual thread

--- src/main/kotlin/com/github/mkorman9/quarkuskotlin/DuckResource.kt
+++ src/main/kotlin/com/github/mkorman9/quarkuskotlin/DuckResource.kt
@@ -1,24 +1,18 @@
 package com.github.mkorman9.quarkuskotlin
 
-import io.smallrye.common.annotation.RunOnVirtualThread
 import jakarta.inject.Inject
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
-import jakarta.ws.rs.Consumes
-import jakarta.ws.rs.GET
-import jakarta.ws.rs.POST
-import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
+import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
 import org.jetbrains.annotations.NotNull
-import java.util.UUID
+import java.util.*
 
 @Path("/api/ducks")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(value = [])
-@RunOnVirtualThread
 class DuckResource {
     @Inject
     lateinit var duckService: DuckService
